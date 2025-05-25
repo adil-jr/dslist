41ce84e1d772bc274216711a8f67a9756a6efa1b
adil-jr <adil.jr@outlook.com>
2025-05-25 07:24:03 -0300

    projections, GET Game by lists

---
+++ src/main/java/com/github/adiljr/dslist/projections/GameMinProjection.java
@@ -0,0 +1,11 @@
+package com.github.adiljr.dslist.projections;
+
+public interface GameMinProjection {
+
+    Long getId();
+    String getTitle();
+    Integer getYear();
+    String getImgUrl();
+    String getShortDescription();
+    Integer getPosition();
+}
--- src/main/java/com/github/adiljr/dslist/services/GameService.java
+++ src/main/java/com/github/adiljr/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.github.adiljr.dslist.services;
 import com.github.adiljr.dslist.dto.GameDTO;
 import com.github.adiljr.dslist.dto.GameMinDTO;
 import com.github.adiljr.dslist.entities.Game;
+import com.github.adiljr.dslist.projections.GameMinProjection;
 import com.github.adiljr.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,12 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
+        return result.stream().map(GameMinDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         return result.stream().map(GameMinDTO::new).toList();
     }
 }
